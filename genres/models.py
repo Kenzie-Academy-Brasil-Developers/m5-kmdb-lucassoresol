a1970cca68b889be767571bbe014edf81fc51d8a
Lucas <lucassoares2007@hotmail.com>
2023-03-02 23:24:40 -0300

    perf: Tarefa 2

--- genres/models.py
+++ genres/models.py
@@ -1,5 +1,11 @@
+import uuid
 from django.db import models
 
 
 class Genre(models.Model):
-    ...
+    id = models.UUIDField(
+        default=uuid.uuid4,
+        primary_key=True,
+        editable=False,
+    )
+    name = models.CharField(max_length=127)
--- movies/views.py
+++ movies/views.py
@@ -1,5 +1,15 @@
 from rest_framework import generics
+from users.permissions import IsAdminUserCreate
+from rest_framework_simplejwt.authentication import JWTAuthentication
+from .models import Movie
+from .serializers import MovieSerializer
 
 
 class MovieView(generics.ListCreateAPIView):
-    ...
+    queryset = Movie.objects.all()
+    serializer_class = MovieSerializer
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [IsAdminUserCreate]
+
+    def perform_create(self, serializer):
+        return serializer.save(user=self.request.user)
