8135bfb99129b540a8d60d4156da30236b7fe20c
Lucas <lucassoares2007@hotmail.com>
2023-03-03 00:33:45 -0300

    perf: Tarefa 3

--- reviews/views.py
+++ reviews/views.py
@@ -1,5 +1,21 @@
+from django.shortcuts import get_object_or_404
 from rest_framework import generics
+from rest_framework_simplejwt.authentication import JWTAuthentication
+from users.permissions import IsUserCreateReview
+from movies.models import Movie
+from .models import Review
+from .serializers import ReviewSerializer
 
 
 class ReviewView(generics.ListCreateAPIView):
-    ...
+    queryset = Review.objects.all()
+    serializer_class = ReviewSerializer
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [IsUserCreateReview]
+
+    def perform_create(self, serializer):
+        movie = get_object_or_404(Movie, id=self.kwargs["movie_id"])
+        return serializer.save(
+            movie=movie,
+            critic=self.request.user,
+        )
--- users/permissions.py
+++ users/permissions.py
@@ -16,3 +16,11 @@ class IsAdminUserCreate(BasePermission):
             return True
 
         return req.user.is_superuser
+
+
+class IsUserCreateReview(BasePermission):
+    def has_permission(self, req: Request, view: View):
+        if req.method != "POST":
+            return True
+
+        return req.user.is_superuser or req.user.is_authenticated and req.user.is_critic
